04c2b25ea7217cea32c24155077a94f4e0cec65a
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-10-07 21:22:35 +0200

    WIP on monads

--- WhatsMonad/Sources/WhatsMonad/WhatsMonad.swift
+++ WhatsMonad/Sources/WhatsMonad/WhatsMonad.swift
@@ -1,37 +1,178 @@
-struct WhatsMonad {
-    var text = "Hello, World!"
+import Foundation
+
+precedencegroup Map {
+    associativity: left
+    higherThan: Apply
 }
 
+precedencegroup Apply {
+    associativity: left
+    higherThan: DefaultPrecedence
+}
+
+precedencegroup FlatMap {
+    associativity: left
+    higherThan: Apply
+}
+
+infix operator <^>: Map
+
+public func <^><A, B>(
+    _ f: @escaping(A) -> B,
+    a: Maybe<A>
+) -> Maybe<B> {
+    a.map(f)
+}
+
+infix operator <*>: Apply
+
+func <*><A, B>(
+    _ f: Maybe<(A) -> B>,
+    a: Maybe<A>
+) -> Maybe<B> {
+    a.apply(f)
+}
+
+func <*><A, B>(
+    _ f: [(A) -> B],
+    a: [A]
+) -> [B] {
+    a.apply(f)
+}
+
+extension Array {
+    func apply<A>(_ fs: [(Element) -> A]) -> [A] {
+        var result = [A]()
+        
+        for f in fs {
+            for element in self.map(f) {
+                result.append(element)
+            }
+        }
+        
+        return result
+    }
+}
+
+public func id<A>(_ a: A) -> A {
+    a
+}
+
+public func apply<A, B>(
+    _ a: A,
+    _ f: (A) -> B
+) -> B {
+    f(a)
+}
+
+infix operator >>-: FlatMap
+
+func >>-<A, B>(_ a: Maybe<A>, f: @escaping(A) -> Maybe<B>) -> Maybe<B> {
+    return a.flatMap(f)
+}
+
+public func pipe<A, B, C>(
+    _ f: @escaping (A) -> B,
+    _ g: @escaping (B) -> C
+)-> (A) -> C {
+    return { a in
+        g(f(a))
+    }
+}
 
 public func incr(_ i: Int) -> Int { i + 1 }
 
-func notreachable() {}
-
-public enum Container<T> {
-    case error
-    case value(T)
+public enum Maybe<A> {
+    case none
+    case value(A)
     
-    public init(_ v: T) {
-         self = .value(v)
-     }
+    public init(_ v: A) {
+        self = .value(v)
+    }
     
-    func map<G>(_ f: @escaping (T) -> G) -> Container<G> {
+    func map<B>(_ f: @escaping (A) -> B) -> Maybe<B> {
         guard case let .value(t) = self else {
-            return .error
+            return .none
         }
         
         return .value(f(t))
     }
+    
+    func apply<B>(_ f: Maybe<((A) -> B)>) -> Maybe<B> {
+        guard case let .value(transform) = f else {
+            return .none
+        }
+        
+        return self.map(transform)
+        
+        //        switch f {
+        //        case .none:
+        //            return .none
+        //        case let .value(tranform):
+        //            return self.map(tranform)
+        //        }
+        
+        //        switch f {
+        //        case .none:
+        //            return .none
+        //        case let .value(transform): // (A) -> B)
+        //            switch self {
+        //            case .none:
+        //                return .none
+        //            case let .value(a):
+        //                return Maybe<B>(transform(a))
+        //            }
+        //        }
+    }
+    
+    func flatMap<B>(_ f: @escaping(A) -> Maybe<B>) -> Maybe<B> {
+        guard case let .value(a) = self else {
+            return .none
+        }
+        
+        switch f(a) {
+        case .none:
+            return .none
+        case let .value(v):
+            return Maybe<B>(v)
+        }
+    }
 }
 
-func map<T, G>(_ f: @escaping (T) -> G) -> (Container<T>) -> Container<G> {
+
+public func zip<A, B, C: Equatable>(_ lhs: Maybe<A>, rhs: Maybe<B>) -> Maybe<C> {
+    fatalError()
+}
+
+public func unwrap<A>(_ lhs: Maybe<A>, rhs: Maybe<A>) throws -> (A, A) {
+    guard
+        case let .value(vLhs) = lhs,
+        case let .value(vRhls) = rhs else {
+        throw NSError(domain: "assertion failed - no value found", code: -1, userInfo: nil)
+    }
+    
+    return (vLhs, vRhls)
+}
+
+func map<A, B>(
+    _ f: @escaping (A) -> B
+) -> (Maybe<A>) -> Maybe<B> {
     return { result in
         switch result {
-        case .error:
-            return .error
+        case .none:
+            return .none
         case let  .value(t):
             return .value(f(t))
         }
     }
 }
 
+public func curry<A, B, C>(
+    _ f: @escaping(A, B) -> C
+) -> (A) -> (B) -> C {
+    return { a in
+        return { b in
+            f(a, b)
+        }
+    }
+}
--- WhatsMonad/Tests/WhatsMonadTests/WhatsMonadTests.swift
+++ WhatsMonad/Tests/WhatsMonadTests/WhatsMonadTests.swift
@@ -2,58 +2,136 @@ import XCTest
 @testable import WhatsMonad
 
 final class WhatsMonadTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(WhatsMonad().text, "Hello, World!")
-    }
     
-    func test_incr() {
-        XCTAssertEqual(incr(1), 2)
+    // MARK: - Functors
+    
+    func test_apply_function_to_container() {
+        func plusThree(_ i: Int) -> Int {
+            i + 3
+        }
+        
+        let result = Maybe<Int>(1).map(plusThree(_:))
+        
+        XCTAssertEqual(unwrap(result), 4)
     }
     
     func test_container_map() {
-        let container = Container<Int>(1)
+        let container = Maybe<Int>(1)
         
-        guard case let .value(value) = container else {
-            fatalError()
-        }
+        let result = container.map { $0 + 1 }
         
-        XCTAssertEqual(value, 1)
-        
-        let incrContainer = container.map { (i: Int) -> Int in
-            i + 1
-        }
-        
-        guard case let .value(v) = incrContainer else {
-            fatalError()
-        }
-        
-        XCTAssertEqual(v, 2)
-        
-        let valueString = container.map { $0 + 1 }.map { String($0) }
-        
-        guard case let .value(s) = valueString else {
-            fatalError()
-        }
-        
-        XCTAssertEqual(s, "2")
-        
-        let fmap = map { (i: Int) -> Int in
-            i + 1
-        }
-        
-        let r = fmap(container)
-        
-        guard case let .value(incrValue) = r else {
-            fatalError()
-        }
-        
-        XCTAssertEqual(incrValue, 2)
+        XCTAssertEqual(unwrap(result), 2)
     }
+    
+    // MARK: - Map laws
+    
+    func test_map_identity() {
+        let container = Maybe<Int>(1)
+        
+        let result = container.map(id)
+        
+        XCTAssertEqual(unwrap(result), 1)
+    }
+    
+    func test_map_composition() {
+        let incr: (Int) -> Int = { v in v + 1 }
+        let square: (Int) -> Int = { v in v * v }
+        
+        let container = Maybe<Int>(1)
+        
+        let incr_square = pipe(incr, square)
+        
+        let result_composition = container
+            .map(incr_square)
+        
+        let result_chaining = container
+            .map(incr)
+            .map(square)
+        
+        let unwrapped = try! unwrap(result_composition, rhs: result_chaining)
+        XCTAssertTrue(unwrapped.0 == unwrapped.1)
+    }
+    
+    func test_container_map_with_operator() {
+        let incr: (Int) -> Int = { $0 + 1 }
+        let square: (Int) -> Int = { $0 * $0 }
+        
+        let result = pipe(square, incr) <^> Maybe<Int>(2)
+        
+        XCTAssertEqual(unwrap(result), 5)
+    }
+    
+    // MARK: - Apply
+    
+    func test_container_apply() {
+        let stringify = Maybe<(Int) -> String> { String($0) }
+        
+        let container: Maybe<Int> = .value(42)
+        
+        let result = container.apply(stringify)
+        
+        XCTAssertEqual(unwrap(result), "42")
+    }
+    
+    func test_container_apply_2() {
+        let result = curry(addition) <^> Maybe(41) <*> Maybe(1)
+        
+        XCTAssertEqual(unwrap(result), 42)
+        
+        let a: Int? = 41
+        let b: Int? = 1
+        
+        if let a = a, let b = b {
+            let result = addition(a, b: b)
+            
+            XCTAssertEqual(result, 42)
+        }        
+    }
+    
+    func test_container_apply_on_array() {
+        let result =  [ { $0 + 3 }, { $0 * 2 } ] <*> [1, 2, 3]
+        
+        XCTAssertEqual(result, [4, 5, 6, 2, 4, 6])
+    }
+    
+    // MARK: - flatMap
+    
+    func test_container_flatMap() {
+        let result = Maybe<Int>.value(2).flatMap(half(_:))
+        
+        switch result {
+        case .none:
+            fatalError()
+        case let .value(v):
+            XCTAssertEqual(v, 1)
+        }
+    }
+    
+    func test_container_flatMap_operator() {
+        let result = Maybe<Int>.value(2) >>- half
+        
+        XCTAssertEqual(unwrap(result), 1)
+    }
+        
+    func test_container_flatMap_operator_() {
+        let result = Maybe(20) >>- half >>- half
+        
+        XCTAssertEqual(unwrap(result), 5)
+    }
+}
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+func half(_ a: Int) -> Maybe<Int> {
+    a % 2 == 0 ? Maybe(a / 2) : Maybe<Int>.none
+}
+
+func addition(_ a: Int, b: Int) -> Int {
+    a + b
+}
+
+func unwrap<A>(_ result: Maybe<A>) -> A {
+    guard case let .value(v) = result else {
+        fatalError()
+    }
+    
+    return v
 }
